1ff515080731c4594e791a78d36c7bcad944d2f3
Netviet <154336093+netvietmedia@users.noreply.github.com>
2025-11-06 16:49:32 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const isProd = process.env.NODE_ENV === 'production'
-
 export default defineConfig({
-  base: isProd ? '/Video-Frame-Extractor/' : '/',
+  base: process.env.NODE_ENV === 'production'
+    ? '/Video-Frame-Extractor/'
+    : '/',
   plugins: [react()],
 })
