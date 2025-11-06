88cb2ad6d035c779875e3d01bb4c89d804c025fc
Netviet <154336093+netvietmedia@users.noreply.github.com>
2025-11-06 18:05:18 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,1 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import { LanguageProvider } from './hooks/useTranslation'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <LanguageProvider>
-      <App />
-    </LanguageProvider>
-  </React.StrictMode>
-)
