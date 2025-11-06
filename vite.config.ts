8d94b7da1eef7d26ac715112a4de2ea00c63ec98
Netviet <154336093+netvietmedia@users.noreply.github.com>
2025-11-06 16:34:13 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// ✅ Cấu hình chuẩn cho Vite + React + GitHub Pages
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
+  const env = loadEnv(mode, '.', '')
     return {
       server: {
         port: 3000,
