6aa429951d4baa0ec068991f9a78711cccb36b50
Netviet <154336093+netvietmedia@users.noreply.github.com>
2025-11-06 16:41:05 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,9 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// ✅ Cấu hình chuẩn cho Vite + React + GitHub Pages
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, '.', '')
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
-});
+const isProd = process.env.NODE_ENV === 'production'
+
+export default defineConfig({
+  base: isProd ? '/Video-Frame-Extractor/' : '/',
+  plugins: [react()],
+})
